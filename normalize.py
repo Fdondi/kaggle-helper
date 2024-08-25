7929e21110c4b8017464ac1698af070660b10428
Francesco Dondi <francesco314@gmail.com>
2024-08-26 01:17:40 +0200

    directly return a pipeline instead of creating a pipeline inside fit and then forwarding to it*.

--- normalize.py
+++ normalize.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder, PowerTransformer
+from sklearn.preprocessing import OneHotEncoder, PowerTransformer, FunctionTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_selector, ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -26,62 +26,46 @@ class DebugTransformer(BaseEstimator, TransformerMixin):
         print(f"First few rows:\n{X[:5]}")
         return X
 
-
-class FrancTransformer(TransformerMixin, BaseEstimator):
-    def __init__(self, min_frequency=100, numerical_strategy='median', categorical_fill_value='NaN', verbose = False, cores=-1, indicator_type=bool):
-        print("init")
-        self._min_frequency = min_frequency
-        self._numerical_strategy = numerical_strategy
-        self._categorical_fill_value = categorical_fill_value
-        self._verbose = verbose
-        self._cores = cores
-        self._indicator_type = indicator_type
-
-    def fit(self, X, y=None):
-        numerical_columns = make_column_selector(dtype_include=['number'])
-        categorical_columns = make_column_selector(dtype_include=['object', 'category'])
-
-        categorical_imputer = SimpleImputer(missing_values=pd.NA, strategy='constant', fill_value=self._categorical_fill_value)
-        numerical_imputer = SimpleImputer(missing_values=pd.NA, strategy=self._numerical_strategy , add_indicator= True)
-
-        imputer = ColumnTransformer(
-            transformers=[
-                ('impute numbers', numerical_imputer, numerical_columns),
-                ('impute strings', categorical_imputer, categorical_columns)
-            ],
+def build_standard_normalizer(min_frequency=100, numerical_strategy='median', categorical_fill_value='NaN', verbose = False, cores=-1, indicator_type=bool):
+    def build_standard_transformer(transformers):
+        return ColumnTransformer(
+            transformers=transformers,
             remainder='passthrough',
-            verbose=self._verbose,
+            verbose=verbose,
             verbose_feature_names_out=False,
-            n_jobs=-self._cores
+            n_jobs=cores
         )
-
-        encoder = OneHotEncoder(min_frequency=self._min_frequency, handle_unknown='ignore', sparse_output=False, dtype=self._indicator_type)
-        scaler = PowerTransformer()
-
-        normalizer = ColumnTransformer(
-            transformers=[
-                ('encode strings', encoder, categorical_columns),
-                ('scale numbers', scaler, numerical_columns)
-            ],
-            remainder='passthrough',
-            verbose=self._verbose,
-            verbose_feature_names_out=False,
-            n_jobs=-self._cores
-        )
-
-        pipeline_steps = [('imputer', imputer)]
-        if self._verbose:
-            pipeline_steps.append(('debug_after_imputer', DebugTransformer("After Imputer")))
-        pipeline_steps.append(('normalizer', normalizer))
-        if self._verbose:
-            pipeline_steps.append(('debug_after_normalizer', DebugTransformer("After Normalizer")))
-        self.pipeline_ = Pipeline(pipeline_steps, verbose=self._verbose)
-
-        self.pipeline_.fit(X)
-        return self
     
-    def transform(self, X):
-        return self.pipeline_.transform(X)
-    
-    def get_feature_names_out(self, input_features=None):
-        return self.pipeline_.get_feature_names_out(input_features)
+    numerical_columns = make_column_selector(dtype_include=['number'])
+    categorical_columns = make_column_selector(dtype_include=['object', 'category'])
+
+    categorical_imputer = SimpleImputer(missing_values=pd.NA, strategy='constant', fill_value=categorical_fill_value)
+    numerical_imputer = SimpleImputer(missing_values=pd.NA, strategy=numerical_strategy , add_indicator= True)
+
+    imputer = build_standard_transformer([
+            ('impute numbers', numerical_imputer, numerical_columns),
+            ('impute strings', categorical_imputer, categorical_columns)
+        ])
+    missingindicator_bool_forcer = build_standard_transformer([
+            ('force_missingindicator_to_bool', 
+             FunctionTransformer(lambda X: X.astype(bool)), 
+             make_column_selector(pattern='^missingindicator_'))
+        ])
+
+    encoder = OneHotEncoder(min_frequency=min_frequency, handle_unknown='ignore', sparse_output=False, dtype=indicator_type)
+    scaler = PowerTransformer()
+
+    normalizer = build_standard_transformer([
+            ('encode strings', encoder, categorical_columns),
+            ('scale numbers', scaler, numerical_columns)
+        ])
+
+    pipeline_steps = [('imputer', imputer), ('bool_forcer', missingindicator_bool_forcer)]
+    if verbose:
+        pipeline_steps.append(('debug_after_imputer', DebugTransformer("After Imputer")))
+    pipeline_steps.append(('normalizer', normalizer))
+    if verbose:
+        pipeline_steps.append(('debug_after_normalizer', DebugTransformer("After Normalizer")))
+    return Pipeline(pipeline_steps, verbose=verbose)
+
+__all__ = ['build_custom_pipeline']
